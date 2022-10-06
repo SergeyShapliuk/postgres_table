44f10d20130e34c91e1035a97e8e30fd6d476e52
Сергей <sergeshapluk@gmail.com>
2022-10-06 17:35:11 +0300

    fix: Preloader.tsx

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import s from "./Preloader.module.css"
-import {RequestStatusType} from "../../utils/types";
+import {useAppSelector} from "../../store/store";
+
 
 
 type PreloaderType = {
-    // status: RequestStatusType
     children:any
 }
 
 export const Preloader = ({ children}: PreloaderType) => {
+    const status = useAppSelector(state => state.tableReducer.status)
     return (
         <div className={s.preloader}>
             {
-                // status === "loading" &&
+                status === "loading" &&
                 <img
                     src={require("../../assets/preloader.gif")}
                     alt='preloader'
